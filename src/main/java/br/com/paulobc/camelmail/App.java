43e8bea4a3a2795db7790cb8d054f00b41738c77
Paulo Batista <paulo.costa@run2biz.com>
2020-12-16 20:03:55 -0300

    imap routebuilder added

--- src/main/java/br/com/paulobc/camelmail/App.java
+++ src/main/java/br/com/paulobc/camelmail/App.java
@@ -1,11 +1,41 @@
 package br.com.paulobc.camelmail;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.jsse.KeyStoreParameters;
+import org.apache.camel.support.jsse.SSLContextParameters;
+import org.apache.camel.support.jsse.TrustManagersParameters;
+
+import br.com.paulobc.camelmail.route.IMAPMailRouteBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        KeyStoreParameters trust_ksp = new KeyStoreParameters();
+        trust_ksp.setResource("/cacerts.jks");
+        trust_ksp.setPassword("changeit");
+        TrustManagersParameters trustp = new TrustManagersParameters();
+        trustp.setKeyStore(trust_ksp);
+
+        SSLContextParameters scp = new SSLContextParameters();
+        scp.setTrustManagers(trustp);
+
+        CamelContext context = new DefaultCamelContext();
+
+        context.setSSLContextParameters(scp);
+        try {
+            context.addRoutes(new IMAPMailRouteBuilder());
+            while (true) {
+                context.start();
+                Thread.sleep(60000 * 2);
+                context.stop();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/br/com/paulobc/camelmail/route/IMAPMailRouteBuilder.java
@@ -0,0 +1,118 @@
+package br.com.paulobc.camelmail.route;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.PollingConsumer;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+public class IMAPMailRouteBuilder extends RouteBuilder {
+    private String imapAddress;
+    private String mailAddress;
+    private String password;
+    private String port;
+    private String imapEndpoint;
+    private boolean ssl;
+
+    public IMAPMailRouteBuilder() {
+        Properties properties = new Properties();
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        try (InputStream resourceStream = classLoader.getResourceAsStream("application.properties")) {
+            properties.load(resourceStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.setImapAddress(properties.getProperty("gmail-imap-address"));
+        this.setMailAddress(properties.getProperty("gmail-username"));
+        this.setPassword(properties.getProperty("gmail-password"));
+        this.setPort(properties.getProperty("gmail-imap-port"));
+        this.setSsl(("true".equals(properties.getProperty("gmail-ssl")) ? true : false));
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(this.isSsl() ? "imaps://" : "imap://").append(this.getImapAddress()).append(":993")
+                .append("?username=").append(this.getMailAddress()).append("&password=").append(this.getPassword())
+                .append("&delete=false").append("&unseen=true").append("&delay=60000").append("&closeFolder=false")
+                .append("&disconnect=false").append("&debugMode=true").append("&fetchSize=35")
+                .append("&alternativeBodyHeader=plain-text").append("&greedy=true");
+        System.out.println(builder.toString());
+        this.setImapEndpoint(builder.toString());
+
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public void setSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        // PollingConsumer pollingConsumer = null;
+
+        // Endpoint endpoint = this.getContext().getEndpoint(this.getImapEndpoint());
+
+        // pollingConsumer = endpoint.createPollingConsumer();
+        // pollingConsumer.start();
+        from(this.getImapEndpoint())
+                /*
+                 * from(pollingConsumer.getEndpoint()).process(new Processor() {
+                 * 
+                 * @Override public void process(Exchange exchange) throws Exception { ;
+                 * 
+                 * }
+                 * 
+                 * })
+                 */
+                .streamCaching().to("file://newmail");
+
+    }
+
+    public String getImapEndpoint() {
+        return imapEndpoint;
+    }
+
+    public void setImapEndpoint(String imapEndpoint) {
+        this.imapEndpoint = imapEndpoint;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getImapAddress() {
+        return imapAddress;
+    }
+
+    public void setImapAddress(String imapAddress) {
+        this.imapAddress = imapAddress;
+    }
+
+}
