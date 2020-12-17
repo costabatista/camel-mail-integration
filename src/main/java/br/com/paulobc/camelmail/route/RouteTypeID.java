5069393360f8216baed8099fa2a9b2d0390bc985
Paulo Batista <paulo.costa@run2biz.com>
2020-12-16 22:17:48 -0300

    add route id control

---
+++ src/main/java/br/com/paulobc/camelmail/route/RouteTypeID.java
@@ -0,0 +1,15 @@
+package br.com.paulobc.camelmail.route;
+
+public enum RouteTypeID {
+    SMTP("smtp"), IMAP("imap");
+
+    private String id;
+    
+    RouteTypeID(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
