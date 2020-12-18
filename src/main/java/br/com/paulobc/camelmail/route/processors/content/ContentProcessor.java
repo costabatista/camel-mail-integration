cc5f93628b3ca7d31471f1db490a772927c9ce79
Paulo Batista <paulo.costa@run2biz.com>
2020-12-18 11:11:42 -0300

    added parser structure to convert mail to json files

--- src/main/java/br/com/paulobc/camelmail/route/processors/content/ContentProcessor.java
+++ src/main/java/br/com/paulobc/camelmail/route/processors/content/ContentProcessor.java
@@ -1,8 +1,12 @@
 package br.com.paulobc.camelmail.route.processors.content;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import br.com.paulobc.camelmail.format.Mail;
 import br.com.paulobc.camelmail.parser.Parser;
 
 public class ContentProcessor implements Processor {
@@ -10,8 +14,22 @@ public class ContentProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         String messageBody = exchange.getMessage().getBody(String.class);
+        String messageFrom = (String) exchange.getMessage().getHeader("from", String.class);
+        String messageSubject = (String) exchange.getMessage().getHeader("subject", String.class);
+        String messageDate = (String) exchange.getMessage().getHeader("date", String.class);
+        
+        Mail mail = new Mail();
 
-        exchange.getMessage().setBody(new Parser().htmlToPlainText(messageBody));
+        mail.setContent(new Parser().htmlToPlainText(messageBody).trim());
+        mail.setFrom(messageFrom.trim());
+        mail.setSubject(messageSubject.trim());
+        mail.setDate(messageDate.trim());
+
+
+        String formattedMessage = new Parser().toJson(mail, mail.getClass());
+        
+
+        exchange.getMessage().setBody(formattedMessage.toString());
 
     }
 
